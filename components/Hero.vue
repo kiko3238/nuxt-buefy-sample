f33428e5dc9400288f26ae196119d9401b8a1697
Yusuke Kikuchi <yusuke.k11@gmail.com>
2020-03-03 22:27:59 +0900

    Add full-height hero.

---
+++ components/Hero.vue
@@ -0,0 +1,19 @@
+<template>
+   <div class="hero is-info is-fullheight">
+   <div class="hero-body">
+      <div class="container">
+         <h1 class="title">
+         Fullheight title
+         </h1>
+         <h2 class="subtitle">
+         Fullheight subtitle
+         </h2>
+      </div>
+   </div>
+   </div>
+</template>
+
+<script>
+
+export default {}
+</script>
