45724770271c4a0cf5566fe5b043ddaf7b947404
Yusuke Kikuchi <yusuke.k11@gmail.com>
2020-03-04 08:25:51 +0900

    Add footer.

--- pages/index.vue
+++ pages/index.vue
@@ -2,16 +2,18 @@
   <section>
     <Hero/>
     <MainMessage/>
+    <Footer/>
   </section>
 </template>
 
 <script>
 import Hero from '@/components/Hero'
 import MainMessage from '@/components/MainMessage'
+import Footer from '@/components/Footer'
 
 export default {
   components: {
-    Hero, MainMessage
+    Hero, MainMessage, Footer
   }
 }
 </script>
