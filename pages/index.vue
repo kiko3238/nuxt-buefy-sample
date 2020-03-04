014d1768bc604f0ad84c89e40af5c416d36cc100
Yusuke Kikuchi <yusuke.k11@gmail.com>
2020-03-04 20:34:08 +0900

    Add product statistics.

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <Hero/>
     <MainMessage/>
     <ProductFeature/>
+    <ProductStats/>
     <Footer/>
   </section>
 </template>
@@ -11,11 +12,12 @@
 import Hero from '@/components/Hero'
 import MainMessage from '@/components/MainMessage'
 import ProductFeature from '@/components/ProductFeature'
+import ProductStats from '@/components/ProductStats'
 import Footer from '@/components/Footer'
 
 export default {
   components: {
-    Hero, MainMessage, ProductFeature, Footer
+    Hero, MainMessage, ProductFeature, ProductStats, Footer
   }
 }
 </script>
